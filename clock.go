13ba42873a018ff0982c06ce2657f1962c51d264
stephanos <github@stephanbehnke.de>
2014-04-23 17:23:24 +0200

    renamed variable clock.Time to clock.Work

--- clock.go
+++ clock.go
@@ -2,34 +2,34 @@ package clock
 
 import "time"
 
-// Clock mirrors the behaviour of Go's time package.
-var Time Clock
+// Work mirrors the behaviour of Go's time package.
+var Work Clock
 
 func init() {
-	Time = New()
+	Work = New()
 }
 
 // Now returns the current local time.
 func Now() time.Time {
-	return Time.Now()
+	return Work.Now()
 }
 
 // Sleep pauses the current goroutine for at least the duration d.
 // A negative or zero duration causes Sleep to return immediately.
 func Sleep(d time.Duration) {
-	Time.Sleep(d)
+	Work.Sleep(d)
 }
 
 // After waits for the duration to elapse and then sends the current time
 // on the returned channel. It is equivalent to NewTimer(d).C.
 func After(d time.Duration) <-chan time.Time {
-	return Time.After(d)
+	return Work.After(d)
 }
 
 // Tick is a convenience wrapper for NewTicker providing access to the
 // ticking channel only.
 func Tick(d time.Duration) <-chan time.Time {
-	return Time.Tick(d)
+	return Work.Tick(d)
 }
 
 // Ticker returns a new Ticker containing a channel that will send the
@@ -37,7 +37,7 @@ func Tick(d time.Duration) <-chan time.Time {
 // the intervals or drops ticks to make up for slow receivers.
 // The duration d must be greater than zero; if not, Ticker will panic.
 func Ticker(d time.Duration) *time.Ticker {
-	return Time.Ticker(d)
+	return Work.Ticker(d)
 }
 
 // Clock provides the functions from the time package.
@@ -67,7 +67,7 @@ type Clock interface {
 	// TODO: At(t time.Time) <-chan time.Time
 }
 
-// Mock represents a manipulable Time. It is concurrent-friendly.
+// Mock represents a manipulable Work. It is concurrent-friendly.
 type Mock interface {
 	Clock
 
